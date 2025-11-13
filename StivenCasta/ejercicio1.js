ede4afd25cf6bc78b738eac2aa1eebea671d25d0
GodBlessedHans <haseveco@gmail.com>
2025-11-13 12:19:46 -0500

    primer ejercicio corregido

--- StivenCasta/ejercicio1.js
+++ StivenCasta/ejercicio1.js
@@ -1,6 +1,5 @@
-// Ejercicio 1: Calcular total de compra
-const precio1 = parseFloat(prompt("Ingrese el precio del primer producto:"));
-const precio2 = parseFloat(prompt("Ingrese el precio del segundo producto:"));
-const precio3 = parseFloat(prompt("Ingrese el precio del tercer producto:"));
-const totalCompra = precio1 + precio2 + precio3;
-console.log("El total de su compra es: $" + totalCompra);
+let prod1 = parseFloat(prompt("Precio del primer producto:"));
+let prod2 = parseFloat(prompt("Precio del segundo producto:"));
+let prod3 = parseFloat(prompt("Precio del tercer producto:"));
+let total = prod1 + prod2 + prod3;
+console.log("El total a pagar es: $" + total.toFixed(2));
