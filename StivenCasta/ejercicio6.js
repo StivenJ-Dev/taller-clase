b091c40c622a152d660917b7cfd6c3293639f9d1
GodBlessedHans <haseveco@gmail.com>
2025-11-13 12:39:00 -0500

    se creo el archivo ejercicio6 y se realizo la actividad

---
+++ StivenCasta/ejercicio6.js
@@ -0,0 +1,6 @@
+/*Quiero un script que salude formalmente a un cliente.
+ Pide el nombre y el apellido del usuario y muestra en
+  consola el mensaje: "Bienvenido, [Nombre Completo]".*/
+  let nombre = prompt("Nombre:");
+let apellido = prompt("Apellido:");
+console.log(`Bienvenido, ${nombre} ${apellido}`);
