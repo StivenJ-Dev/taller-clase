81dcdd1e49c5d4dc9c5601a6f6f57d298149b475
GodBlessedHans <haseveco@gmail.com>
2025-11-13 12:13:58 -0500

    se realizo el primer ejercicio para calcular el total de las compras

---
+++ StivenCasta/ejercicio1.js
@@ -0,0 +1,6 @@
+// Ejercicio 1: Calcular total de compra
+const precio1 = parseFloat(prompt("Ingrese el precio del primer producto:"));
+const precio2 = parseFloat(prompt("Ingrese el precio del segundo producto:"));
+const precio3 = parseFloat(prompt("Ingrese el precio del tercer producto:"));
+const totalCompra = precio1 + precio2 + precio3;
+console.log("El total de su compra es: $" + totalCompra);
