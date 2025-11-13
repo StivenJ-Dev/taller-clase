95b05fbb80fdabfb6d7205ab621f6ea0b10c92f9
GodBlessedHans <haseveco@gmail.com>
2025-11-13 12:30:54 -0500

    se realizo el ejercicio para saber la edad del usuario

---
+++ StivenCasta/ejercicio5.js
@@ -0,0 +1,5 @@
+/*Necesito saber la edad de un usuario. Pide su año de nacimiento y,
+ asumiendo que el año actual es 2024, calcula cuántos años tiene.*/
+ let nacimiento = parseInt(prompt("Año de nacimiento:"));
+let edad = 2024 - nacimiento;
+console.log("Tu edad es: " + edad + " años");
